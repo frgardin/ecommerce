6cfc3e1f539fde17bec6bae84cd311cc5abc5fb9
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-11-27 22:42:29 -0300

    init project

---
+++ src/test/java/org/gardin/felipe/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gardin.felipe.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
