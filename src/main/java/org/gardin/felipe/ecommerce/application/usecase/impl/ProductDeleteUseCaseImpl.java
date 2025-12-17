60188eecefbffadb01bfaece83e0973e4c4425f9
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-12-16 23:23:49 -0300

    implement delete

--- src/main/java/org/gardin/felipe/ecommerce/application/usecase/impl/ProductDeleteUseCaseImpl.java
+++ src/main/java/org/gardin/felipe/ecommerce/application/usecase/impl/ProductDeleteUseCaseImpl.java
@@ -5,7 +5,7 @@ import org.gardin.felipe.ecommerce.application.usecase.DeleteUseCase;
 
 public record ProductDeleteUseCaseImpl(ProductGateway productGateway) implements DeleteUseCase<Long> {
     @Override
-    public void execute(Long aLong) {
-
+    public void execute(Long id) {
+        productGateway.delete(id);
     }
 }
